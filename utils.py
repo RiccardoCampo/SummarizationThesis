3a6f480c986ac1b0740712beecc53c5e486857ca
Riccardo <riccardo.cmp94@yahoo.it>
2018-09-14 17:22:23 +0200

    reformat execution files

--- utils.py
+++ utils.py
@@ -9,6 +9,9 @@ import nltk
 import tensorflow as tf
 import tensorflow_hub as hub
 import numpy as np
+
+import matplotlib
+matplotlib.use("Pdf")
 import matplotlib.pyplot as plt
 
 from nltk import word_tokenize, PorterStemmer
