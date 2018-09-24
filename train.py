2be1128bb5e26e793a799afad7be598ed3930dac
Riccardo <riccardo.cmp94@yahoo.it>
2018-09-24 10:28:03 +0200

    nyt training, larger training set

--- train.py
+++ train.py
@@ -23,7 +23,7 @@ def train(series_name, batch_size, epochs, binary, dataset, weights=None):
         doc_size = 300
         duc_index = 0
         #indices = permutation(35)
-        indices = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+        indices = [19, 22, 27, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
                    10, 11, 12, 13, 14, 15, 16, 17, 18,
                    #19,
                    20, 21,
